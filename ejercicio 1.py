6200ab02297da0ee5c3729811e603e91e67d79cc
MARGARITA9530 <margaritaaa18@gmail.com>
2025-06-05 16:42:12 -0500

    se subio el proyecto

---
+++ ejercicio 1.py
@@ -0,0 +1,9 @@
+# Solicitar el número de horas trabajadas y el costo por hora
+horas = int(input("Ingrese el número de horas trabajadas: "))
+costo_por_hora = float(input("Ingrese el costo por hora: "))
+
+# Calcular el pago total
+pago_total = horas * costo_por_hora
+
+# Mostrar el resultado
+print(f"El pago que le corresponde es: ${pago_total}")
